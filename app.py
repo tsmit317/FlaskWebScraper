9394bd3f15d6b4c6f709d28b734207b7a68bdee8
Taylor Smith <tjsmith1513@gmail.com>
2021-03-17 12:31:36 -0400

    heroku added sched start to main

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def sched_job():
      sys.stdout.flush()
 
 
-scheduler.start()    
+
 
 
 @app.route('/')
@@ -105,5 +105,5 @@ if __name__ == '__main__':
     db.create_all()
     # delete_everthing(ResortDB)
     # update_db()
-       
+    scheduler.start()    
     app.run(debug=True)    
